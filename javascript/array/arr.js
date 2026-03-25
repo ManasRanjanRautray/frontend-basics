1b0119ab286cb3d53008e222bcf6ad25f769a731
Manas Ranjan Rautray <manasranjanrautray76@gmail.com>
2026-03-25 14:22:48 +0530

    array in js

---
+++ javascript/array/arr.js
@@ -0,0 +1,9 @@
+//there are no array in js it's called object
+// let mark1=98;
+// let mark2=89;
+// let mark3=67;
+// let mark4=62;
+// let mark5 =23;
+ let mark=[98,89,67,62,23]
+ console.log(mark);
+ console.log(mark.length);
